f92edd68a9600cc8e4886392b423e19ede8ba070
Matt Gregg <mattdgregg@gmail.com>
2019-02-05 23:03:43 -0600

    Fix the monad font

--- src/utils/typography.jsx
+++ src/utils/typography.jsx
@@ -1,35 +1,26 @@
-import Typography from "typography"
+import Typography from 'typography';
 
 const typography = new Typography({
-  baseFontSize: "18px",
-  bodyFontFamily: ["Montserrat", "Sans-serif"],
+  baseFontSize: '18px',
+  bodyFontFamily: ['Montserrat', 'Sans-serif'],
   scaleRatio: 2.222,
-  headerFontFamily: [
-    "Monad",
-    "Helvetica",
-    "Arial",
-    "sans-serif",
-  ],
+  headerFontFamily: ['Monad', 'Helvetica', 'Arial', 'sans-serif'],
   headerWeight: 500,
   googleFonts: [
     {
       name: 'Montserrat',
-      styles: [
-        '400',
-        '500',
-        '700',
-      ],
-    }
-   ],
-   overrideStyles: () => ({
-      h1: {
-        fontFamily: ['Montserrat', 'sans-serif'].join(','),
-      },
-      img: {
-        marginBottom: 0,
-      }
-    })
-  })
+      styles: ['400', '500', '700'],
+    },
+  ],
+  overrideStyles: () => ({
+    h1: {
+      fontFamily: ['Montserrat', 'sans-serif'].join(','),
+    },
+    img: {
+      marginBottom: 0,
+    },
+  }),
+});
 
-export default typography
-export const rhythm = typography.rhythm
+export default typography;
+export const rhythm = typography.rhythm;
