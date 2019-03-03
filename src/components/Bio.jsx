4cfd678cad0c70e85f245ca82344191af3ec4a21
Matt Gregg <mattdgregg@gmail.com>
2019-03-02 22:39:40 -0700

    Add tweet button

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,47 +1,47 @@
 import React from 'react';
 
-const Bio = () => (
-  <div>
-    <p>
-      Written by <strong>Matt Gregg</strong>, an engineer who lives and works in
-      Minneapolis, MN
-    </p>
-    <p>
-      Have something to say about this post? Tweet at me{' '}
-      <a
-        href='https://twitter.com/intent/tweet?screen_name=itwasmattgregg&ref_src=twsrc%5Etfw'
-        class='twitter-mention-button'
-        data-text='Yo'
-        data-show-count='true'
-      >
-        Tweet @itwasmattgregg
-      </a>
-      <script
-        async
-        src='https://platform.twitter.com/widgets.js'
-        charset='utf-8'
-      />
-    </p>
-  </div>
-);
+export default class Bio extends React.Component {
+  componentDidMount() {
+    window.twttr = (function(d, s, id) {
+      let js;
+      const fjs = d.getElementsByTagName(s)[0];
+      const t = window.twttr || {};
+      if (d.getElementById(id)) return t;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://platform.twitter.com/widgets.js';
+      fjs.parentNode.insertBefore(js, fjs);
 
-// componentDidMount() {
-//   var scriptNode = document.getElementById('twitter-wjs')
-//   if (scriptNode) {
-//     scriptNode.parentNode.removeChild(scriptNode)
-//   }
+      t._e = [];
+      t.ready = function(f) {
+        t._e.push(f);
+      };
 
-//   !function(d,s,id){
-//     var js,
-//         fjs=d.getElementsByTagName(s)[0],
-//         p=/^http:/.test(d.location)?'http':'https';
-//     if(!d.getElementById(id)){
-//       js=d.createElement(s);
-//       js.id=id;
-//       js.src=p+'://platform.twitter.com/widgets.js';
-//       fjs.parentNode.insertBefore(js,fjs);
-//     }
-//   }(document, 'script', 'twitter-wjs');
-// }
+      return t;
+    })(document, 'script', 'twitter-wjs');
 
-export default Bio;
+    console.log(document.getElementsByTagName('title')[0].innerHTML);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>
+          Written by <strong>Matt Gregg</strong>, an engineer who lives and
+          works in Minneapolis, MN
+        </p>
+        <p>
+          Have something to say about this post? Tweet at me{' '}
+          <a
+            href='https://twitter.com/intent/tweet?screen_name=itwasmattgregg&ref_src=twsrc%5Etfw'
+            className='twitter-mention-button'
+            data-text={`${window.location}`}
+            data-show-count='true'
+          >
+            @itwasmattgregg
+          </a>
+        </p>
+      </div>
+    );
+  }
+}
