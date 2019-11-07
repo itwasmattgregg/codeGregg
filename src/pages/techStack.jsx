aa3639201366eccc2f5ecdf495c7e475fb3cddb4
Matt Gregg <mattdgregg@gmail.com>
2019-11-07 11:31:04 -0600

    updates to everything!

---
+++ src/pages/techStack.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import get from 'lodash/get';
+import { Helmet } from 'react-helmet';
+
+import Layout from '../components/layout/layout';
+import Container from '../components/container';
+
+class TechStack extends React.Component {
+  render() {
+    const siteTitle = get(this.props, 'data.site.siteMetadata.title');
+
+    return (
+      <Layout>
+        <Helmet title={`Tech Stack | ${siteTitle}`} />
+        <h1 className={`text_center`}>Tech Stack</h1>
+        <Container>
+          <p>This is a place for my thoughts</p>
+        </Container>
+      </Layout>
+    );
+  }
+}
+
+export default TechStack;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
