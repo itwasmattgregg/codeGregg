d795c005e315e946a8fcd658a7ca4d0ab0c0de2b
Matt Gregg <mattdgregg@gmail.com>
2020-05-05 13:56:34 -0500

    Fix helmet import

--- src/templates/category.jsx
+++ src/templates/category.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import get from 'lodash/get';
 import { graphql, Link } from 'gatsby';
 import Layout from '../components/layout/layout';
