b9051627278e791decbd7430e842b8c80e491aa8
Matt Gregg <mattdgregg@gmail.com>
2020-05-09 17:38:46 -0500

    add rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
         short_name: 'codeGregg',
         start_url: '/',
         background_color: '#ffffff',
-        theme_color: 'green',
+        theme_color: '#1A344D',
         display: 'minimal-ui',
         icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
@@ -88,6 +88,67 @@ module.exports = {
       },
     },
     `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.parent.fields.slug,
+                  guid:
+                    site.siteMetadata.siteUrl + edge.node.parent.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                });
+              });
+            },
+            query: `
+              {
+                allMdx {
+                  edges {
+                    node {
+                      id
+                      excerpt
+                      html
+                      parent {
+                        ... on File {
+                          fields {
+                            slug
+                          }
+                        }
+                      }
+                      frontmatter {
+                        title
+                        tags
+                        visible
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/rss.xml',
+            title: "Your Site's RSS Feed",
+          },
+        ],
+      },
+    },
     {
       resolve: 'gatsby-plugin-typography',
       options: {
