d1e83a8f39ea5b3df8da15ad9aadcd386557ab74
Matt Gregg <mattdgregg@gmail.com>
2024-09-24 23:02:12 -0500

    Revert "Add pnpm"
    This reverts commit 123457b1d705dc0b8fb619fdedee67ceaf94c3f9.

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
+    'gatsby-plugin-offline',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
